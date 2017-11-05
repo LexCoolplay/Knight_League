34f9450fa0fe3d807c0a09fd51e18bf76d4c4c82
LexCoolplay <30982713+LexCoolplay@users.noreply.github.com>
2017-11-05 13:29:21 +0300

    Add files via upload

--- J.py
+++ J.py
@@ -4,23 +4,33 @@ k = 0
 i=""
 length=0
 for _ in range(N):
-    i+= input()[:-5]
+    i+= input().strip()[:-5]
 for j in i.split():
-    if (length + len(j)+4 <= 280):
+    if (len(res[k]) + len(j)+4 <= 280):
+        print(len(res[k]) + len(j)+4,k)
         length+=len(j)+4
         res[k] += j + " "
-    elif(length+len(j)>280):
+    elif(len(res[k])+len(j)+4>280):
         res[k]+="("+str(k+1)+"/"
         res.append("")
         k+=1
-for a in range(len(res)):
-    if(res[a][-5]!='('):
-        res[a] += '('+str(k+1)+'/'
-    res[a] += str(k+1) + ")"
 
+for a in range(len(res)):
+    if(res[a] and res[a][-3]!='('):
+        res[a] += '('+str(k+1)+'/'
+    if(res[a]):
+        res[a] += str(k+1) + ")"
+print(k)
 print(*res,sep="\n")
 
 
-# 2
-# Winter!... The peasant breathes a sign, (1/2)
-# renews his sledge, and makes his way. (2/2)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
+#
