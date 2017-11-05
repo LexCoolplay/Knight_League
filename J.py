2c80bbf6d8c25bc268f43897b0804404aa6fff8d
LexCoolplay <30982713+LexCoolplay@users.noreply.github.com>
2017-11-05 15:23:33 +0300

    Update J.py

--- J.py
+++ J.py
@@ -11,11 +11,9 @@ i=textwrap.wrap(i,280)
 
 for a in range(len(i)):
     i[a]+=" ("+str(a+1)+'/'+str(len(i))+")"
+    i[a]=i[a].replace("  "," ")
 print(len(i))
 print(*i,sep="\n")
-
-
-
 
 #aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
 #aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(1/9)
