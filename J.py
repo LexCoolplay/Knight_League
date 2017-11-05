bc7dc6959e368a6914288abb02b5c43c141740d4
LexCoolplay <30982713+LexCoolplay@users.noreply.github.com>
2017-11-05 15:26:54 +0300

    Update J.py

--- J.py
+++ J.py
@@ -1,27 +1,27 @@
 N = int(input())
 res = [""]
 k = 0
-i=""
-length=0
+i = ""
+length = 0
 for _ in range(N):
-    inp=input().strip()
-    i+= inp[:inp.rfind('(')]+" "
+    inp = input().strip()
+    i += inp[:inp.rfind('(')] + " "
 for j in i.split():
-    if (len(res[k]) + len(j)+6 <= 280):
-       # print(len(res[k]) + len(j)+4,k)
-        length+=len(j)+4
+    if (len(res[k]) + len(j) + len('(' + str(k + 1) + '/'+str(k + 1) + ")") <= 280):
+        # print(len(res[k]) + len(j)+4,k)
+        length += len(j) + 4
         res[k] += j + " "
-    elif(len(res[k])+len(j)+6>280):
-        res[k]+="("+str(k+1)+"/"
+    elif (len(res[k]) + len(j) + len('(' + str(k + 1) + '/'+str(k + 1) + ")") > 280):
+        res[k] += "(" + str(k + 1) + "/"
         res.append("")
-        k+=1
- 
+        k += 1
+
 for a in range(len(res)):
-    if(len(res[a])<3 and res[a] or len(res[a])>=3 and res[a][-3]!='('):
-        res[a] += '('+str(k+1)+'/'
-    if(res[a]):
-        res[a] += str(k+1) + ")"
-if(N<=0):
-    k-=1
-print(k+1)
-print(*res,sep="\n")
+    if (len(res[a]) < 3 and res[a] or len(res[a]) >= 3 and res[a][-3] != '('):
+        res[a] += '(' + str(k + 1) + '/'
+    if (res[a]):
+        res[a] += str(k + 1) + ")"
+if (N <= 0):
+    k -= 1
+print(k + 1)
+print(*res, sep="\n")
