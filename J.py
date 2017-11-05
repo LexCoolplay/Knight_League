018125cb5e74c8fb410f0642048a00c47728b02a
LexCoolplay <30982713+LexCoolplay@users.noreply.github.com>
2017-11-05 16:44:03 +0300

    Update J.py

--- J.py
+++ J.py
@@ -3,21 +3,28 @@ res = [""]
 k = 0
 i = ""
 length = 0
+from functools import reduce
+N = int(input())
+res = [""]
+k = 0
+i = ""
+length = 0
 
 for _ in range(N):
     inp = input().strip()
     i += inp[:inp.rfind('(')]
-Array_length=list(map(lambda x:len(x),i.split()))
-COUNT=sum(Array_length)//280
-for j in i.split():
-    if (len(res[k]) + len(j) + len('(' + str(k + 1) + '/'+str(COUNT) + ")")+1 <= 280):
-        # print(len(res[k]) + len(j)+4,k)
-        length += len(j) + 4
-        res[k] += j + " "
-    elif (len(res[k]) + len(j) + len('(' + str(k + 1) + '/'+str(COUNT) + ")")+1 > 280):
-        res[k] += "(" + str(k + 1) + "/"
-        res.append("")
-        k += 1
+for COUNT in range(5,10):
+    for j in i.split():
+        if (len(res[k]) + len(j) + COUNT+1 <= 280):
+            # print(len(res[k]) + len(j)+4,k)
+            length += len(j) + 4
+            res[k] += j + " "
+        elif (len(res[k]) + len(j) + COUNT+1 > 280):
+            res[k] += "(" + str(k + 1) + "/"
+            res.append("")
+            k += 1
+    if(not reduce(lambda x,y:x or (len(y)>=280-COUNT),res, False)):
+        break;
 
 for a in range(len(res)):
     if (len(res[a]) < 3 and res[a] or len(res[a]) >= 3 and res[a].rfind('(')==-1 ):
