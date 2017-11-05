48360f110d0c02e8bae1b73086732020219216b0
LexCoolplay <30982713+LexCoolplay@users.noreply.github.com>
2017-11-05 16:08:22 +0300

    Update J.py

--- J.py
+++ J.py
@@ -3,15 +3,18 @@ res = [""]
 k = 0
 i = ""
 length = 0
+
 for _ in range(N):
     inp = input().strip()
-    i += inp[:inp.rfind('(')] + " "
+    i += inp[:inp.rfind('(')]
+Array_length=list(map(lambda x:len(x),i.split()))
+COUNT=sum(Array_length)//280
 for j in i.split():
-    if (len(res[k]) + len(j) + len('(' + str(k + 1) + '/'+str(k + 1) + ")")+1 <= 280):
+    if (len(res[k]) + len(j) + len('(' + str(k + 1) + '/'+str(COUNT) + ")")+1 <= 280):
         # print(len(res[k]) + len(j)+4,k)
         length += len(j) + 4
         res[k] += j + " "
-    elif (len(res[k]) + len(j) + len('(' + str(k + 1) + '/'+str(k + 1) + ")")+1 > 280):
+    elif (len(res[k]) + len(j) + len('(' + str(k + 1) + '/'+str(COUNT) + ")")+1 > 280):
         res[k] += "(" + str(k + 1) + "/"
         res.append("")
         k += 1
